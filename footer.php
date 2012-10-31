5db6f13b14b2b6c999f10f75ffb67cb863f719b1
Ryan Imel <ryan@aspiringindie.com>
2012-10-31 11:20:20 -0400

    Began footer styling.

--- footer.php
+++ footer.php
@@ -14,9 +14,8 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'wpcandy_theme_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wpcandy_theme' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wpcandy_theme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( '%1$s by %2$s with %3$s.', 'wpcandy_theme' ), 'WPCandy Theme', '<a href="http://ryanimel.com/" rel="designer">Ryan Imel</a>', 'many kudos' ); ?>
+			<p id="proudly"><a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wpcandy_theme' ); ?>" rel="generator"><?php printf( __( 'Proudly powered, of course, by %s', 'wpcandy_theme' ), 'WordPress' ); ?></a></p>
+			<p id="credits"><?php printf( __( '%1$s by %2$s with %3$s.', 'wpcandy_theme' ), 'WPCandy Theme', '<a href="http://ryanimel.com/" rel="designer">Ryan Imel</a>', 'many kudos' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon .site-footer -->
 </div><!-- #page .hfeed .site -->
