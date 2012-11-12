e5ff466dd418ea85a040745fa870b59d9fe57fa8
Ryan Imel <ryan@aspiringindie.com>
2012-11-12 02:36:45 -0500

    Added support for link format posts.

--- content.php
+++ content.php
@@ -7,18 +7,42 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
-	<?php get_template_part( 'templates/blog', 'taxonomies' ); ?>
-	
-	<header class="entry-header">
+	<?php 
+	// If this is a link formatted post.
+	if ( has_post_format( 'link', get_the_ID() ) ) { ?>
 		
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'wpcandy_theme' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<header class="entry-header">
+			
+			<p class="post-permalink"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark">Permalink</a></p>
+			
+			<?php $wpcandy_link = get_post_custom_values( 'linkformaturl', get_the_ID() ); ?>
+			<?php if (isset($wpcandy_link[0])) { ?>
+				<h2 class="entry-title"><a href="<?php echo $wpcandy_link[0]; ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+			<?php } else { ?>
+				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			<?php } ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php wpcandy_theme_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+			<?php if ( 'post' == get_post_type( get_the_ID() ) ) : ?>
+				<div class="entry-meta">
+					<?php wpcandy_theme_posted_on(); ?>
+				</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header-><!-- .entry-header -->
+		
+	<?php } else { ?>	
+		<?php get_template_part( 'templates/blog', 'taxonomies' ); ?>
+	
+		<header class="entry-header">
+		
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'wpcandy_theme' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+
+			<?php if ( 'post' == get_post_type() ) : ?>
+				<div class="entry-meta">
+					<?php wpcandy_theme_posted_on(); ?>
+				</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
+	<?php } ?>
 
 	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
 	<div class="entry-summary">
